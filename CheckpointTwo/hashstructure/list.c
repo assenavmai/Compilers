0572e74f7997493ad44b1e2fcdc9b1f27bc778dc
Vanessa White <whitevanessa14@gmail.com>
2017-03-16 01:16:44 -0400

    Adding files for list and hash

---
+++ CheckpointTwo/hashstructure/list.c
@@ -0,0 +1,7 @@
+/************************************************
+* File: list.c
+* Linked list functions
+* Authors: Jasmine Kandloosi and Vanessa White
+* March 2017
+*************************************************/
+
---
+++ CheckpointTwo/hashstructure/list.h
@@ -0,0 +1,14 @@
+/************************************************
+* File: list.h 
+* linked list data structure specification
+* Authors: Jasmine Kandloosi and Vanessa White
+* March 2017
+*************************************************/
+
+
+#ifndef _LIST_H
+#define _LIST_H_
+
+
+
+#endif
