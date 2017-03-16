3e917e8da3f6af4a800cd6dd74e41f52e00c4c7b
Vanessa White <whitevanessa14@gmail.com>
2017-03-16 00:13:51 -0400

    Adding starting point for Checkpoint2

---
+++ CheckpointTwo/main.c
@@ -0,0 +1,94 @@
+/************************************************
+* File: main.c 
+* Main program for CMinus
+* Authors: Jasmine Kandloosi and Vanessa White
+* February 2017
+*************************************************/
+#define STDOUT FALSE
+
+#include "globals.h"
+#include "util.h"
+#include "scan.h"
+#include "parse.h"
+
+/* allocate global variables */
+int lineno = 0;
+FILE * source;
+FILE * listing;
+FILE * code;
+
+int main(int argc, char const *argv[])
+{
+	struct TreeNode * tree;
+	char sourceFilename[120];
+	char outFile[120];
+	char * ext;
+	char ch;
+	int i = 0;
+	int j = 0;
+
+	if(argc < 3)
+	{
+		fprintf(stderr,"usage: %s [-a] <filename>\n",argv[0]);
+		exit(1);
+	}
+
+	strcpy(sourceFilename, argv[2]);
+	ext = strchr(sourceFilename, '.');
+
+	/* Making sure the file is a valid file */
+	if(!ext)
+	{
+		fprintf(stderr, "Error: File has no extension\n");
+		exit(1);
+	}
+	else
+	{
+		if(strcmp(ext +1, "cm") != 0)
+		{
+			printf("Error: Extension has to be '.cm'\n");
+			exit(1);
+		}
+	}
+
+	ch = sourceFilename[i];
+	while(ch != '.') //get the source file name to add the '.abs' to it
+	{
+		outFile[j++] = ch;
+		i++;
+		ch = sourceFilename[i];
+	}
+	outFile[j++] = '\0';
+
+
+	if(strcmp(argv[1], "-a") == 0) // print the abstract syntax tree to the file
+	{
+		strcat(outFile, ".abs");
+
+		source = fopen(sourceFilename, "r"); // file with the code
+
+		if(!source)
+		{
+			fprintf(stderr, "Error: File %s not found\n", sourceFilename);
+			exit(1);
+		}
+		
+		#if STDOUT
+			listing = stdout;
+
+		#else
+		    listing = fopen(outFile, "w");	// file with abstract syntax tree
+			if(!listing)
+			{
+				fprintf(stderr, "Error: File %s not found\n", outFile);
+				exit(1);
+			}
+		#endif
+
+		tree = parse();
+		fprintf(listing,"\nSyntax tree:\n");
+		printTree(tree);
+	}
+	
+	return 0;
+}
---
+++ CheckpointTwo/scan.h
@@ -0,0 +1,36 @@
+/************************************************
+* File: scan.h 
+* Scanner interface for CMinus
+* Authors: Jasmine Kandloosi and Vanessa White
+    Originally provided by Fei Song
+* February 2017
+*************************************************/
+
+#ifndef _SCAN_H_
+#define _SCAN_H_
+
+/* MAXTOKENLEN is the maximum size of a token */
+#define MAXTOKENLEN 40
+
+/* tokenString array stores the lexeme of each token */
+extern char tokenString[MAXTOKENLEN+1];
+extern char idString[MAXTOKENLEN+1];
+extern char numString[MAXTOKENLEN+1];
+
+
+
+
+/**
+* [FUNCTION PROTOTYPE]
+* Used in: [list files]
+*
+* [Line what the function does]
+* 
+* @param name: description
+* @param name: description
+* @param name: description
+*
+* @return
+**/
+
+#endif
